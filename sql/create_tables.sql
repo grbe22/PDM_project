1df03aaf7c1d48652787aeb941b9e65264e42dce
Will Poirier <wmp9980@rit.edu>
2024-10-10 09:31:29 -0400

    fixed create tables syntax

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,18 +1,18 @@
-CREATE TABLE user(
+CREATE TABLE users(
     user_id INT PRIMARY KEY,
     username VARCHAR(16) UNIQUE,
     email VARCHAR(100) UNIQUE,
     password VARCHAR(30) UNIQUE,
     first_name VARCHAR(16),
     last_name VARCHAR(16),
-    creation_date DATETIME,
-    last_access_time DATETIME
+    creation_date TIMESTAMP,
+    last_access_time TIMESTAMP
 );
 
 CREATE TABLE following(
     follow_id INT PRIMARY KEY,
-    follower_id INT FOREIGN KEY uid REFERENCES user.user_id,
-    following_id INT FOREIGN KEY uid REFERENCES user.user_id
+    follower_id INT REFERENCES user.user_id,
+    following_id INT REFERENCES user.user_id
 );
 
 CREATE TABLE genre(
@@ -38,11 +38,11 @@ CREATE TABLE publisher(
 
 CREATE TABLE playtime(
     play_id INT PRIMARY KEY,
-    user_id INT FOREIGN KEY user_id REFERENCES user.user_id,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
-    start_time DATETIME,
-    end_time DATETIME,
-    uc1 CONSTRAINT (user_id, start_time) UNIQUE
+    user_id INT REFERENCES user.user_id,
+    game_id INT REFERENCES game.game_id,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
+    CONSTRAINT uc1 UNIQUE (user_id, start_time)
 );
 
 CREATE TABLE platform(
@@ -52,57 +52,57 @@ CREATE TABLE platform(
 
 CREATE TABLE release(
     release_id INT PRIMARY KEY,
-    platform_id INT FOREIGN KEY platform_id REFERENCES platform.platform_id,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
+    platform_id INT REFERENCES platform.platform_id,
+    game_id INT REFERENCES game.game_id,
     price FLOAT(3,2),
-    release_date DATETIME
+    release_date DATE
 );
 
 CREATE TABLE publishing(
     publishing_id INT PRIMARY KEY,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
-    publisher_id INT FOREIGN KEY publisher_id REFERENCES publisher.publisher_id
-    uc1 CONSTRAINT (game_id, publisher_id) UNIQUE
+    game_id INT REFERENCES game.game_id,
+    publisher_id INT REFERENCES publisher.publisher_id,
+    CONSTRAINT uc1 UNIQUE (game_id, publisher_id)
 );
 
 CREATE TABLE developing(
     developing_ID INT PRIMARY KEY,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
-    developer_id INT FOREIGN KEY developer_id REFERENCES developer.developer_id,
-    uc1 CONSTRAINT (game_id, developer_id) UNIQUE
+    game_id INT REFERENCES game.game_id,
+    developer_id INT REFERENCES developer.developer_id,
+    CONSTRAINT uc1 UNIQUE (game_id, developer_id)
 );
 
 CREATE TABLE game_genre(
     game_genre_id INT PRIMARY KEY,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
-    genre_id INT FOREIGN KEY genre_id REFERENCES genre.genre_id,
-    uc1 CONSTRAINT (game_id, genre_id) UNIQUE
+    game_id INT REFERENCES game.game_id,
+    genre_id INT REFERENCES genre.genre_id,
+    CONSTRAINT uc1 UNIQUE (game_id, genre_id)
 );
 
 CREATE TABLE collection(
     collection_id INT PRIMARY KEY,
     name VARCHAR(50),
-    user_id INT FOREIGN KEY user_id REFERENCES user.user_id
+    user_id INT REFERENCES user.user_id
 );
 
 CREATE TABLE rating(
     rating_id INT PRIMARY KEY,
-    user_id INT FOREIGN KEY user_id REFERENCES user.user_id,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
+    user_id INT REFERENCES user.user_id,
+    game_id INT REFERENCES game.game_id,
     rating INT,
-    rating_range CONSTRAINT (rating >= 1 and rating <= 5)
+    CONSTRAINT rating_range CHECK (rating >= 1 and rating <= 5)
 );
 
 CREATE TABLE platform_owned_by_user(
     platform_owned_by_user_id INT PRIMARY KEY,
-    user_id INT FOREIGN KEY user_id REFERENCES user.user_id,
-    platform_id INT FOREIGN KEY platform_id REFERENCES platform.platform_id,
-    uc1 CONSTRAINT (user_id, platform_id) UNIQUE,
+    user_id INT REFERENCES user.user_id,
+    platform_id INT REFERENCES platform.platform_id,
+    CONSTRAINT uc1 UNIQUE (user_id, platform_id)
 );
 
 CREATE TABLE game_in_collection(
     game_in_collection_id INT PRIMARY KEY,
-    game_id INT FOREIGN KEY game_id REFERENCES game.game_id,
-    collection_id INT FOREIGN KEY collection_id REFERENCES collection.collection_id,
-    uc1 CONSTRAINT (game_id, collection_id) UNIQUE
+    game_id INT REFERENCES game.game_id,
+    collection_id INT REFERENCES collection.collection_id,
+    CONSTRAINT uc1 UNIQUE (game_id, collection_id)
 );
